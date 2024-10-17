1594cdfe20474633acd1b1b28884e33e071b2976
OliverHELP <oliverhwinkler@gmail.com>
2024-10-17 01:34:32 -0700

    Lab 4 update and javascript comments.

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - loss wax bronze casting and hangman pseudocode
+// Author: Oliver Winkler <oliverhwinkler@gmail.com>
+// Date: 10/17/2024
+
+// SIMPLE COMPUTER GAME OUTLINE:
+// Creating a Hangman game:
+//    Generate a word at random
+//    For each letter, print an underscore
+//      Hide letters from user
+//    Print post for hanging figure
+//    Print English alphabet for user
+//    User provides input by clicking letters from the alphabet
+//      If input is incorrect--not a letter or in the word--print figure part "n"
+//      If input is correct, print all letters that are the same as the input
+//    When all letters in the word are validated, flash victory screen
+//      If "n" equals 6, flash lose screen
+
+
 
 // Constants
 
