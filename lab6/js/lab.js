a8a1e92d8bb89cc9df6a8152d2990605c260ea8b
OliverHELP <oliverhwinkler@gmail.com>
2024-10-23 17:26:51 -0700

    lab 6 and javascript

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - lab 6 Arrays and Objects
+// Author: Oliver Winkler
+// Date:10/23/2024
 
 // Constants
 
@@ -16,6 +16,29 @@ function main() {
   console.log("Main function started.");
   // the code that makes everything happen
 }
+
+
+//Define Variables
+
+myTransport = [" bus", " car" , " plane"]
+
+myMainRide = {
+
+  make: "Toyota",
+  model: "tacoma",
+  year: 2015,
+  age: function () {
+      return 2024-this.year;
+  }
+}
+
+// output
+document.writeln("Getting around: " + myTransport + "<br>")
+
+
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
 
 // let's get this party started
 main();
