b8fefe4e5bcfba2e4dab2cece6e4437a4de8ca42
OliverHELP <oliverhwinkler@gmail.com>
2024-12-02 23:27:17 -0800

    lab16 update. works

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -3,37 +3,29 @@
 // Date:12/2/2024
 
 
+function update(){
+
 
 // Using the core $.ajax() method
 $.ajax({
     // The URL for the request (from the api docs)
-    url: "https://xkcd.com/info.0.json/",
-    // The data to send (will be converted to a query string)
-    comicObj: {
-        // here is where any data required by the api 
-        //   goes (check the api docs)
-        month: "12",
-        num: 3019,
-        link: "",
-        year: "2024",
-        news: "",
-        safe_title: "Advent Calendar Advent Calendar",
-        transcript: "",
-        alt: "The growth rate of items per day may may seem absurd, but it's actually much less than the acceleration in the 12 Days of Christmas song.",
-        img: "https://imgs.xkcd.com/comics/advent_calendar_advent_calendar.png",
-        title: "Advent Calendar Advent Calendar",
-        day: "2"
-    },
+    url: " https://api.waifu.pics/sfw/dance",
+
     // Whether this is a POST or GET request
     type: "GET",
     // The type of data we expect back
     dataType: "json",
     // What do we do when the api call is successful
     //   all the action goes in here
-    success: function (comicObj) {
+    success: function (data) {
         // do stuff
         //$("#output").append(comicObj.img);
-        $("#output").append("<h3>" + comicObj.title + "</h3>" + "<img>" + comicObj.img + "</img>" + comicObj.alt);
+        $("#output").append(
+            $("<img>", {
+                src: data.url,
+                class: "uniform"
+            })
+        );
     },
     // What we do if the api call fails
     error: function (jqXHR, textStatus, errorThrown) {
@@ -43,3 +35,9 @@ $.ajax({
     }
 })
 
+}
+$("#get").click(function(){
+
+update();
+
+});
